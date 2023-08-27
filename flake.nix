35e48dce8c12e119bf995db8a4dc50c31a35ebb1
Robin Appelman <robin@icewind.nl>
2023-08-27 16:03:41 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/release-23.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+  utils.lib.eachDefaultSystem (system: let
+      overlays = [];
+      pkgs = (import nixpkgs) {
+        inherit system overlays;
+      };
+      nodejs = pkgs.nodejs_18;
+    in rec {
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [nodejs.pkgs.node-pre-gyp nodejs.pkgs.node-gyp-build pkg-config which nodePackages.typescript python3];
+        buildInputs = with pkgs; [libjpeg pixman cairo pango postgresql];
+      };
+    });
+}
